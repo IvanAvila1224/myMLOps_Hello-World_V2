2fa2cac80e669fe7c340dc9d33d873642bc01758
Ivan Avila <104231414+IvanAvila1224@users.noreply.github.com>
2023-11-20 14:49:23 -0600

    Update linear-model.py

--- linear-model.py
+++ linear-model.py
@@ -1,4 +1,4 @@
-# adsoft 
+
 import numpy as np
 import os
 #import matplotlib.pyplot as plt
@@ -8,27 +8,25 @@ import tensorflow as tf
  
 print(tf.__version__)
 
-X = np.arange(-10.0, 10.0, 1e-2)
+X = np.arange(1, 11, 1)  # Cambiamos la generación de X para que no sea mayor a 10
 np.random.shuffle(X)
-y =  10.0 * X + 0.0
+y = 2.0 * X + 5.0  # Cambiamos la fórmula aquí
 
 train_end = int(0.6 * len(X))
-#print (train_end)
 test_start = int(0.8 * len(X))
-#print (test_start)
 X_train, y_train = X[:train_end], y[:train_end]
 X_test, y_test = X[test_start:], y[test_start:]
 X_val, y_val = X[train_end:test_start], y[train_end:test_start]
 
 tf.keras.backend.clear_session()
 linear_model = tf.keras.models.Sequential([
-                                           tf.keras.layers.Dense(units=1, input_shape=[1], name='Single')
-                                           ])
+    tf.keras.layers.Dense(units=1, input_shape=[1], name='Single')
+])
 linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.mean_squared_error)
 print(linear_model.summary())
 
 linear_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20)
-print(linear_model.predict([ [0.0], [2.0], [3.1], [4.2], [5.2] ] ).tolist() )   
+print(linear_model.predict([[1.0], [2.0], [3.0], [4.0], [5.0]]).tolist())   
 
 export_path = 'linear-model/1/'
-tf.saved_model.save(linear_model, os.path.join('./',export_path))
+tf.saved_model.save(linear_model, os.path.join('./', export_path))
