344c4d47e1afc5dc5e99ff5f599aaf3702087abc
Ivan Avila <104231414+IvanAvila1224@users.noreply.github.com>
2023-11-20 15:14:27 -0600

    Update model_client.py

--- model_client.py
+++ model_client.py
@@ -2,11 +2,11 @@ import json
 import streamlit as st
 import requests
 
-# URL del servidor que ejecuta el modelo linear-model y usa la interfaz de predicción.
+
 SERVER_URL = 'https://linear-model-service-ivanavila1224.cloud.okteto.net/v1/models/linear-model:predict'
 
 def make_prediction(inputs):
-    # Preparar la solicitud de predicción en el formato JSON esperado.
+   
     predict_request = {'instances': [inputs]}
     response = requests.post(SERVER_URL, json=predict_request)
     response.raise_for_status()
@@ -16,10 +16,10 @@ def make_prediction(inputs):
 def main():
     st.title('Estimador de Tiempo de Desarrollo')
 
-    # Interfaz de usuario para ingresar el tamaño del código.
+   
     code_size = st.number_input('Ingrese el numero de lineas de codigo:', min_value=0.0, step=1.0)
 
-    # Realizar la predicción basada en el tamaño del código ingresado.
+   
     if st.button('Predecir'):
         prediction = make_prediction([code_size])
         st.write(f'Tiempo estimado de desarrollo para un tamaño de código en horas de {code_size}: {prediction["predictions"][0][0]}')
